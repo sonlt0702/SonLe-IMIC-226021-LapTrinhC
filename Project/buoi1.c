d9685504484f90ae745461a2a7bfc5110ebcde2f
Le Son <sonlethanhauto@gmail.com>
2026-04-11 11:13:59 +0700

    buoi1

---
+++ Project/buoi1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void my_funtion() {
+	printf("Hello Class \n");
+}
--- Project/main.c
+++ Project/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+void my_funtion();
 
 void main() {
 	printf("Hello World \n");
+	my_funtion();
 }
