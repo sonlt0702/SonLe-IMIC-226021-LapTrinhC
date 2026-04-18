0362aaa277e9177749263ac51d40f391f4342323
Le Son <sonlethanhauto@gmail.com>
2026-04-18 11:37:58 +0700

    buoi 2

--- Project/libarary.h
+++ Project/libarary.h
@@ -1,2 +1,7 @@
 #pragma once
 void my_funtion();
+void giai_pt_bac_2();
+void bai_1();
+void bai_2();
+void bai_3();
+void bai_4();
--- Project/main.c
+++ Project/main.c
@@ -2,6 +2,18 @@
 #include "libarary.h"
 
 void main() {
-	printf("Hello World \n");
-	my_funtion();
+	char isContinue = 'y';
+	while (isContinue == 121) {
+		//printf("Hello World \n");
+		//my_funtion();
+		//giai_pt_bac_2();
+		//bai_1();
+		//bai_2();
+		//bai_3();
+		bai_4();
+
+		printf("do you want to continue (y/n): ");
+		scanf_s(" %c", &isContinue);
+	}
+	printf("ket thuc chuong trinh");
 }
