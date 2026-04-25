066eda2c014f31c2196182dc59a33ca6ed67a0b8
Le Son <sonlethanhauto@gmail.com>
2026-04-25 21:32:57 +0700

    buoi 3

--- Project/main.c
+++ Project/main.c
@@ -2,18 +2,17 @@
 #include "libarary.h"
 
 void main() {
-	char isContinue = 'y';
-	while (isContinue == 121) {
-		//printf("Hello World \n");
-		//my_funtion();
-		//giai_pt_bac_2();
-		//bai_1();
-		//bai_2();
-		//bai_3();
-		bai_4();
-
-		printf("do you want to continue (y/n): ");
-		scanf_s(" %c", &isContinue);
-	}
-	printf("ket thuc chuong trinh");
+	//printf("Hello World \n");
+	//my_funtion();
+	//giai_pt_bac_2();
+	//bai_1();
+	//bai_2();
+	//bai_3();
+	//buoi3_bai_2();
+	//buoi3_bai_2();
+	//buoi3_bai_3();
+	//buoi3_bai_4();
+	//buoi3_bai_5();
+	//buoi3_bai_6();
+	buoi3_bai_7();
 }
